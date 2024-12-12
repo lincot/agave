431e1c4f6b9cf674c9f363689221db3bf48a98e4
Brooks <brooks@anza.xyz>
2024-12-12 14:38:26 -0500

    Reclaims more old accounts in `clean` (#4044)
    (cherry picked from commit 3d438241544121386b88e654d5f6a666df294365)
    
    # Conflicts:
    #	accounts-db/src/accounts_db.rs
    #	accounts-db/src/accounts_db/tests.rs

--- accounts-db/src/accounts_db.rs
+++ accounts-db/src/accounts_db.rs
@@ -2792,6 +2792,19 @@ impl AccountsDb {
                                         } else {
                                             found_not_zero += 1;
                                         }
+
+                                        // If this candidate has multiple rooted slot list entries,
+                                        // we should reclaim the older ones.
+                                        if slot_list.len() > 1
+                                            && *slot
+                                                <= max_clean_root_inclusive.unwrap_or(Slot::MAX)
+                                        {
+                                            should_collect_reclaims = true;
+                                            purges_old_accounts_local += 1;
+                                            useless = false;
+                                        }
+                                        // Note, this next if-block is only kept to maintain the
+                                        // `uncleaned_roots_slot_list_1` stat.
                                         if uncleaned_roots.contains(slot) {
                                             // Assertion enforced by `accounts_index.get()`, the latest slot
                                             // will not be greater than the given `max_clean_root`
@@ -2800,12 +2813,16 @@ impl AccountsDb {
                                             {
                                                 assert!(slot <= &max_clean_root_inclusive);
                                             }
+<<<<<<< HEAD
                                             if slot_list.len() > 1 {
                                                 // no need to purge old accounts if there is only 1 slot in the slot list
                                                 should_purge = true;
                                                 purges_old_accounts_local += 1;
                                                 useless = false;
                                             } else {
+=======
+                                            if slot_list.len() == 1 {
+>>>>>>> 3d43824154 (Reclaims more old accounts in `clean` (#4044))
                                                 self.clean_accounts_stats
                                                     .uncleaned_roots_slot_list_1
                                                     .fetch_add(1, Ordering::Relaxed);
